484274feebd9f6391869b634c75fceeb93eeaf3d
alizaLakhani14 <49196299+alizaLakhani14@users.noreply.github.com>
2019-07-13 15:24:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,497 @@
+let foodsinn{
+    noOfbranches:3,
+    branches: [
+        {
+            city:"karachi",
+            area:"sindhi-muslim",
+            staff: [
+                head-chef: {
+    name: "Arshad",
+        expertise: "continental",
+            qualification: "diploma-in-culinary-arts",
+                age: 0,
+                    email: "arshad@gmail.com",
+                        facebookPage: "chef-arshad",
+                            experience: "12-years",
+                                salary: 100000,
+            
+                }
+juniorChefs: [
+    "armia", "aliza", "farheen", "mohd", "hassan"
+]
+waiters: [
+    {
+        name: "ali-akber"
+                        salary: 10000,
+        contactNo: 00000000,
+        age: 0,
+        experience: "fresher",
+        gender: "male",
+        designation: "junior-waiter"
+
+    }
+                    {
+        name: "danish"
+                        salary: 10000,
+        contactNo: 00000000,
+        age: 0,
+        experience: "2years",
+        gender: "male",
+        designation: "junior-waiter"
+    }
+                    {
+        name: "daniyal"
+                        salary: 10000,
+        contactNo: 00000000,
+        age: 0,
+        experience: "1years",
+        gender: "male",
+        designation: "junior-waiter"
+    }
+                    {
+        name: "bilal"
+                        salary: 10000,
+        contactNo: 00000000,
+        age: 0,
+        experience: "5years",
+        gender: "male",
+        designation: "junior-waiter"
+    }
+                    {
+        name: "Ahsan"
+                        salary: 10000,
+        contactNo: 00000000,
+        age: 0,
+        experience: "2years",
+        gender: "male",
+        designation: "senior-waiter"
+    }
+
+]
+managementCommitee: [
+    {
+        name: "batool",
+        designation: "deputy-manager",
+        salary: 50000,
+        gender: "female",
+        contactNumber: 00000,
+        age: 0,
+    }
+                    {
+        name: "yasir",
+        designation: "manager",
+        salary: 75000,
+        gender: "male",
+        contactNumber: 00000,
+        age: 0,
+    }
+                    {
+        name: "shariq",
+        designation: "coordinator",
+        salary: 45000,
+        gender: "male",
+        contactNumber: 00000,
+        age: 0,
+    }
+
+]
+gateKeeper: {
+    name: "khan",
+        age: 40,
+            salary: 25000,
+                shift: "day",
+                    
+                }
+gateKeeper: {
+    name: "waqas",
+        age: 38,
+            salary: 25000,
+                shift: "night",
+                    
+                }
+            ]
+        }
+hasFreeDelivery: true,
+    hasValetParking: true,
+        onFoodPanda: true,
+            onEatOye: true,
+                onEatMubarak: true,
+                    {
+                        city: "Islamabad",
+                        area: "defence",
+                        staff: [
+                            head - chef: {
+                                name: "natasha",
+                                expertise: "Pakistani-Indian",
+                                qualification: "diploma-in-culinary-arts",
+                                age: 0,
+                                email: "arshad@gmail.com",
+                                facebookPage: "chef-arshad",
+                                experience: "12-years",
+                                salary: 100000,
+
+                            }
+                juniorChefs: [
+                                "armia", "aliza", "farheen", "mohd", "hassan"
+                            ]
+                waiters: [
+                                {
+                                    name: "wali"
+                        salary: 10000,
+                                    contactNo: 00000000,
+                                    age: 0,
+                                    experience: "fresher",
+                                    gender: "male",
+                                    designation: "junior-waiter"
+
+                                }
+                    {
+                                    name: "sadiq"
+                        salary: 10000,
+                                    contactNo: 00000000,
+                                    age: 0,
+                                    experience: "2years",
+                                    gender: "male",
+                                    designation: "junior-waiter"
+                                }
+                    {
+                                    name: "Raza"
+                        salary: 10000,
+                                    contactNo: 00000000,
+                                    age: 0,
+                                    experience: "1years",
+                                    gender: "male",
+                                    designation: "junior-waiter"
+                                }
+                    {
+                                    name: "asad"
+                        salary: 10000,
+                                    contactNo: 00000000,
+                                    age: 0,
+                                    experience: "5years",
+                                    gender: "male",
+                                    designation: "junior-waiter"
+                                }
+                    {
+                                    name: "raza"
+                        salary: 10000,
+                                    contactNo: 00000000,
+                                    age: 0,
+                                    experience: "2years",
+                                    gender: "male",
+                                    designation: "senior-waiter"
+                                }
+
+                            ]
+                managementCommitee: [
+                                {
+                                    name: "abid",
+                                    designation: "deputy-manager",
+                                    salary: 50000,
+                                    gender: "female",
+                                    contactNumber: 00000,
+                                    age: 0,
+                                }
+                    {
+                                    name: "yaseen",
+                                    designation: "manager",
+                                    salary: 75000,
+                                    gender: "male",
+                                    contactNumber: 00000,
+                                    age: 0,
+                                }
+                    {
+                                    name: "shahid",
+                                    designation: "coordinator",
+                                    salary: 45000,
+                                    gender: "male",
+                                    contactNumber: 00000,
+                                    age: 0,
+                                }
+
+                            ]
+                gateKeeper: {
+                                name: "raxa",
+                                age: 40,
+                                salary: 25000,
+                                shift: "day",
+
+                            }
+                gateKeeper: {
+                                name: "meesum",
+                                age: 38,
+                                salary: 25000,
+                                shift: "night",
+
+                            }
+
+                        ]
+            hasFreeDelivery: true,
+                        hasValetParking: true,
+                        onFoodPanda: true,
+                        onEatOye: true,
+                        onEatMubarak: true,
+                    }
+city: "murree",
+    area: "nazimabad",
+        staff: [
+            head - chef: {
+                name: "Saqib",
+                expertise: "Chinese",
+                qualification: "diploma-in-culinary-arts",
+                age: 0,
+                email: "saqib@gmail.com",
+                facebookPage: "chef-saqib",
+                experience: "12-years",
+                salary: 100000,
+
+            }
+                juniorChefs: [
+                "armia", "aliza", "farheen", "mohd", "hassan"
+            ]
+                waiters: [
+                {
+                    name: "kumail"
+                        salary: 10000,
+                    contactNo: 00000000,
+                    age: 0,
+                    experience: "fresher",
+                    gender: "male",
+                    designation: "junior-waiter"
+
+                }
+                    {
+                    name: "kaiser"
+                        salary: 10000,
+                    contactNo: 00000000,
+                    age: 0,
+                    experience: "2years",
+                    gender: "male",
+                    designation: "junior-waiter"
+                }
+                    {
+                    name: "waqas"
+                        salary: 10000,
+                    contactNo: 00000000,
+                    age: 0,
+                    experience: "1years",
+                    gender: "male",
+                    designation: "junior-waiter"
+                }
+                    {
+                    name: "gilani"
+                        salary: 10000,
+                    contactNo: 00000000,
+                    age: 0,
+                    experience: "5years",
+                    gender: "male",
+                    designation: "junior-waiter"
+                }
+                    {
+                    name: "asim"
+                        salary: 10000,
+                    contactNo: 00000000,
+                    age: 0,
+                    experience: "2years",
+                    gender: "male",
+                    designation: "senior-waiter"
+                }
+
+            ]
+                managementCommitee: [
+                {
+                    name: "asif",
+                    designation: "deputy-manager",
+                    salary: 50000,
+                    gender: "female",
+                    contactNumber: 00000,
+                    age: 0,
+                }
+                    {
+                    name: "rizwan",
+                    designation: "manager",
+                    salary: 75000,
+                    gender: "male",
+                    contactNumber: 00000,
+                    age: 0,
+                }
+                    {
+                    name: "shah-jee",
+                    designation: "coordinator",
+                    salary: 45000,
+                    gender: "male",
+                    contactNumber: 00000,
+                    age: 0,
+                }
+
+            ]
+                gateKeeper: {
+                name: "shahvez",
+                age: 40,
+                salary: 25000,
+                shift: "day",
+
+            }
+                gateKeeper: {
+                name: "shahzain",
+                age: 38,
+                salary: 25000,
+                shift: "night",
+
+            }
+
+        ]
+hasFreeDelivery: true,
+    hasValetParking: true,
+        onFoodPanda: true,
+            onEatOye: true,
+                onEatMubarak: true,
+        }
+    ]
+
+cusines: [
+    {
+        name: "pakistani",
+        dishes: [
+            {
+                name: "nihari",
+                smallPrice: 500,
+                largePrice: 1000,
+                options: ["beef", "chicken", "mutton"]
+
+            }
+            {
+                name: "bheja-fry",
+                smallPrice: 500,
+                largePrice: 1000,
+                options: ["beef", "mutton"]
+            }
+            {
+                name: "biryani",
+                smallPrice: 500,
+                largePrice: 1000,
+                options: ["beef", "chicken", "mutton", "prawn"]
+            }
+            {
+                name: "pulao",
+                smallPrice: 500,
+                largePrice: 1000,
+                options: ["beef", "chicken", "mutton", "prawn"]
+            }
+            {
+                name: "handi",
+                smallPrice: 500,
+                largePrice: 1000,
+                options: ["beef", "chicken", "mutton",]
+            }
+            {
+                name: "cheeseHandi",
+                smallPrice: 500,
+                largePrice: 1000,
+                options: ["beef", "chicken", "mutton",]
+            }
+            {
+                name: "GreenChicken",
+                smallPrice: 500,
+                largePrice: 1000,
+                options: ["beef", "chicken", "mutton", "prawn"]
+            }
+            {
+                name: "bihariChicken",
+                smallPrice: 500,
+                largePrice: 1000,
+                options: ["beef", "chicken", "mutton", "prawn"]
+            }
+        ]
+        {
+        name: "barbeque",
+        dishes: [
+            {
+                name: "tikka",
+                price: 300,
+            }
+            ,
+            {
+                name: "malai-boti",
+                price: 350,
+            }
+            ,
+            {
+                name: "malai-tikka",
+                price: 350,
+            }
+            ,
+            {
+                name: "bihari-boti",
+                price: 400,
+            }
+            ,
+            {
+                name: "bihariTikka",
+                price: 300,
+            }
+            ,
+            {
+                name: "kaleji",
+                price: 300,
+            }
+            ,
+            {
+                name: "seekhKabab",
+                price: 300
+            }
+            ,
+            {
+                name: "fishTikka",
+                price: 300
+            }
+            ,
+            {
+                name: "mixPlatter",
+                price: 1000,
+            }
+            ,
+            {
+                name: "seafoodBarbquePlatter",
+                price: 2000,
+            }
+        ]
+    }
+    
+    {
+        name: "chinese"
+dishes: [
+            {
+                name: "chowmein",
+                price: 400
+            }
+            ,
+            {
+                name: "shashlik",
+                price: 350
+            }
+            ,
+            {
+                name: "shashlik",
+                price: 350
+            }
+            ,
+            {
+                name: "chicken-manchurian",
+                price: 400
+            }
+            ,
+            {
+                name: "fried-rice",
+                price: 400
+            }
+
+
+
+
+
+        ]
+    }
+    }
+]
+}
